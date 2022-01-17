532398404a731bc8d22eef0e518831e3ec964bd9
Joris Smolders <joris_smolders@outlook.com>
2022-01-17 14:57:12 +0100

    New initialize

---
+++ DuranceBagKataGitHubTests/BagTests.cs
@@ -0,0 +1,45 @@
+using DuranceBagKataGitHub;
+using DuranceBagKataGitHub.Items;
+using System;
+using Xunit;
+
+namespace DuranceBagKataGitHubTests
+{
+    public class BagTests
+    {
+        [Fact]
+        public void AddItemToBackpack()
+        {
+            // Arrange
+            var d = new Durance();
+
+            // Act
+            d.Find(new Iron());
+            d.Find(new Copper());
+
+            // Assert
+            Assert.Equal(2, d.Backpack.Items.Count);
+        }
+
+        [Fact]
+        public void AddNineItemsToBackpack()
+        {
+            // Arrange
+            var d = new Durance();
+
+            // Act
+            d.Find(new Iron());
+            d.Find(new Copper());
+            d.Find(new Copper());
+            d.Find(new Copper());
+            d.Find(new Copper());
+            d.Find(new Copper());
+            d.Find(new Copper());
+            d.Find(new Iron());
+            d.Find(new Copper());
+
+            // Assert
+            Assert.Equal(8, d.Backpack.Items.Count);
+        }
+    }
+}
